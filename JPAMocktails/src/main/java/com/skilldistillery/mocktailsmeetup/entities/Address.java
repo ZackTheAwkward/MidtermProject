642f8ac2d4351dbc4adf39b1891ee720de141bc3
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-04-22 15:12:05 -0500

    Stubbed Address and Category entity

---
+++ JPAMocktails/src/main/java/com/skilldistillery/mocktailsmeetup/entities/Address.java
@@ -0,0 +1,104 @@
+package com.skilldistillery.mocktailsmeetup.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String street;
+	
+	
+	private String city;
+	
+	@Column(name="state_abbr")
+	private String stateAbbr;
+	
+	@Column(name="postal_code")
+	private String postalCode;
+	
+
+	public Address() {
+		super();
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Address: " + street + ", " + city + ", " + stateAbbr + ", " + postalCode;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStateAbbr() {
+		return stateAbbr;
+	}
+
+	public void setStateAbbr(String stateAbbr) {
+		this.stateAbbr = stateAbbr;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		return id == other.id;
+	}
+	
+	
+	
+	
+	
+}
