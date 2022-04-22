aa108c325c7de4a9442f4ffd4fb1d9a877fbbdc7
Desiree <desiree.vanassche@gmail.com>
2022-04-22 15:30:11 -0600

    Added junit testing

---
+++ JPAMocktails/src/test/java/com/skilldistillery/mocktailsmeetup/entities/RecipeTest.java
@@ -0,0 +1,56 @@
+package com.skilldistillery.mocktailsmeetup.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RecipeTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Recipe recipe;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		  emf = Persistence.createEntityManagerFactory("JPAMocktails");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		   emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		 em = emf.createEntityManager();
+		 recipe = em.find(Recipe.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		recipe = null;
+		em.close();
+	}
+	
+
+	@Test
+	void Test1() {
+	assertNotNull(recipe);
+	assertEquals("CHANGE ME", recipe.getName());
+	assertEquals("CHANGE ME", recipe.getDescription());
+	assertEquals("CHANGE ME", recipe.getServingSize());
+	assertEquals("CHANGE ME", recipe.getCreateDate());
+
+	
+		
+	}
+	
+	
+	}
