2325265df954c29ca94a991e12d206bedefad725
Desiree <desiree.vanassche@gmail.com>
2022-04-22 14:22:48 -0600

    Address corrected

--- JPAMocktails/src/main/java/com/skilldistillery/mocktailsmeetup/entities/Address.java
+++ JPAMocktails/src/main/java/com/skilldistillery/mocktailsmeetup/entities/Address.java
@@ -10,71 +10,34 @@ import javax.persistence.Id;
 
 @Entity
 public class Address {
-<<<<<<< HEAD
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Id
-	private Integer id;
-	
-	@Column
-	private String street;
-	
-	@Column
-	private String city;
-	
-=======
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
-	private String street;
-	
-	
-	private String city;
->>>>>>> 31b692b508eabadf97e3336ae53ed6ba89e9a939
-	
-	@Column(name="state_abbr")
-	private String stateAbbr;
-	
-	@Column(name="postal_code")
-	private String postalCode;
-	
-<<<<<<< HEAD
-// ----- END Fields -----------------------------
-	
-=======
 
->>>>>>> 31b692b508eabadf97e3336ae53ed6ba89e9a939
+	private String street;
+
+	private String city;
+
+	@Column(name = "state_abbr")
+	private String stateAbbr;
+
+	@Column(name = "postal_code")
+	private String postalCode;
+
+// ----- END Fields -----------------------------
+
 	public Address() {
 		super();
 	}
+
+// --- END no-arg Constructor-----------------
 	
-<<<<<<< HEAD
-// --- END no-arg Constructor----------------- 	
-	
-
-	public Integer getId() {
-		return id;
-	}
-
-
-	public void setId(Integer id) {
-=======
-	
-
-	@Override
-	public String toString() {
-		return "Address: " + street + ", " + city + ", " + stateAbbr + ", " + postalCode;
-	}
-
-
-
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
->>>>>>> 31b692b508eabadf97e3336ae53ed6ba89e9a939
 		this.id = id;
 	}
 
@@ -112,7 +75,7 @@ public class Address {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		return Objects.hash(city, id, postalCode, stateAbbr, street);
 	}
 
 	@Override
@@ -124,24 +87,16 @@ public class Address {
 		if (getClass() != obj.getClass())
 			return false;
 		Address other = (Address) obj;
-<<<<<<< HEAD
-		return Objects.equals(id, other.id);
+		return Objects.equals(city, other.city) && id == other.id && Objects.equals(postalCode, other.postalCode)
+				&& Objects.equals(stateAbbr, other.stateAbbr) && Objects.equals(street, other.street);
 	}
 
 	@Override
 	public String toString() {
 		return "Address [id=" + id + ", street=" + street + ", city=" + city + ", stateAbbr=" + stateAbbr
 				+ ", postalCode=" + postalCode + "]";
-=======
-		return id == other.id;
->>>>>>> 31b692b508eabadf97e3336ae53ed6ba89e9a939
 	}
 	
 	
-	
-<<<<<<< HEAD
-=======
-	
-	
->>>>>>> 31b692b508eabadf97e3336ae53ed6ba89e9a939
+
 }
