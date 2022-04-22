80cfc8e4d551e0e04d61c7e7f14975c7282085b7
Desiree <desiree.vanassche@gmail.com>
2022-04-22 15:15:05 -0600

    Junit testing

--- JPAMocktails/src/test/java/com/skilldistillery/mocktailsmeetup/entities/AddressTest.java
+++ JPAMocktails/src/test/java/com/skilldistillery/mocktailsmeetup/entities/AddressTest.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.mocktailsmeetup.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,46 +11,48 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
-
+class AddressTest {
 	private static EntityManagerFactory emf;
 	private EntityManager em;
-	private User user; 
-	
-	
+	private Address address;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-	    emf = Persistence.createEntityManagerFactory("JPAMocktails");
+		  emf = Persistence.createEntityManagerFactory("JPAMocktails");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-	    emf.close();
+		   emf.close();
 	}
 
-	
 	@BeforeEach
 	void setUp() throws Exception {
-	    em = emf.createEntityManager();
-	    user = em.find(User.class, 1);
+		 em = emf.createEntityManager();
+		    address = em.find(Address.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-	    em.close();
-	    user = null;
+		address = null;
+		em.close();
 	}
 	
-	
+
 	@Test
-	@DisplayName("initial test")
-	void test1 () {
-		assertNotNull(user);
-		assertEquals("Andre5000", user.getUsername());			
+	void Test1() {
+	assertNotNull(address);
+	assertEquals("Shirley Street", address.getStreet());
+	assertEquals("CO spring", address.getCity());
+	assertEquals("CO", address.getStateAbbr());
+	assertEquals("11111", address.getPostalCode());
+	
+		
 	}
 	
 	
-}
+	}
+
+
