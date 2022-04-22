6c15ec411f11f3c14191e2ffa8d41b168fa77153
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-04-22 15:36:49 -0500

    Stubbed out meetup entity

---
+++ JPAMocktails/src/main/java/com/skilldistillery/mocktailsmeetup/entities/Address.java
@@ -0,0 +1,102 @@
+package com.skilldistillery.mocktailsmeetup.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String street;
+
+	private String city;
+
+	@Column(name = "state_abbr")
+	private String stateAbbr;
+
+	@Column(name = "postal_code")
+	private String postalCode;
+
+// ----- END Fields -----------------------------
+
+	public Address() {
+		super();
+	}
+
+// --- END no-arg Constructor-----------------
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStateAbbr() {
+		return stateAbbr;
+	}
+
+	public void setStateAbbr(String stateAbbr) {
+		this.stateAbbr = stateAbbr;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, id, postalCode, stateAbbr, street);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		return Objects.equals(city, other.city) && id == other.id && Objects.equals(postalCode, other.postalCode)
+				&& Objects.equals(stateAbbr, other.stateAbbr) && Objects.equals(street, other.street);
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", city=" + city + ", stateAbbr=" + stateAbbr
+				+ ", postalCode=" + postalCode + "]";
+	}
+	
+	
+
+}
