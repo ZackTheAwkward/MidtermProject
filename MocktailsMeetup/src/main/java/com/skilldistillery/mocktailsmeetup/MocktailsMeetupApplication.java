727145f588b8dd3f50109b4243b3e3f1153eefd1
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-04-22 09:59:09 -0600

    stubbed out Spring Boot MVC project, add project dependency

---
+++ MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/MocktailsMeetupApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mocktailsmeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocktailsMeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocktailsMeetupApplication.class, args);
+	}
+
+}
---
+++ MocktailsMeetup/src/test/java/com/skilldistillery/mocktailsmeetup/MocktailsMeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mocktailsmeetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocktailsMeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
