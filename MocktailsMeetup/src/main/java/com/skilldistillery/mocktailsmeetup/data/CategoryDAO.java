01f20b58d4d42fcbfb864d13133cf796b586d7ab
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-04-24 11:08:49 -0600

    Stubbed out keyword search for category and drink

--- MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/data/CategoryDAO.java
+++ MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/data/CategoryDAO.java
@@ -1,7 +1,11 @@
 package com.skilldistillery.mocktailsmeetup.data;
 
+import java.util.List;
+
 import com.skilldistillery.mocktailsmeetup.entities.Category;
 
 public interface CategoryDAO {
 	Category findById(int categoryId);
+	List<Category> findByKeyword(String keyword);
+	
 }
--- MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/data/DrinkDaoImpl.java
+++ MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/data/DrinkDaoImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.mocktailsmeetup.data;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -14,10 +16,18 @@ public class DrinkDaoImpl implements DrinkDAO {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Override
 	public Drink findById(int drinkId) {
-		return em.find(Drink.class, drinkId) ;
+		return em.find(Drink.class, drinkId);
+	}
+
+	@Override
+	public List<Drink> findByKeyword(String keyword) {
+		List<Drink> drinks = null;
+		String jpql = "SELECT d FROM Drink d WHERE d.name = :keyword OR d.description = :keyword";
+		drinks = em.createQuery(jpql, Drink.class).setParameter("keyword", "%" + keyword + "%" ).getResultList();
+		return drinks;
 	}
 
 }
