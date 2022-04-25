bb999734a2ed586129d745921a92310ded26e819
Desiree <desiree.vanassche@gmail.com>
2022-04-25 09:15:05 -0600

    CRUD operations added for Address

--- MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/data/AddressDAO.java
+++ MocktailsMeetup/src/main/java/com/skilldistillery/mocktailsmeetup/data/AddressDAO.java
@@ -1,7 +1,20 @@
 package com.skilldistillery.mocktailsmeetup.data;
 
 import com.skilldistillery.mocktailsmeetup.entities.Address;
+import com.skilldistillery.mocktailsmeetup.entities.Meetup;
 
 public interface AddressDAO {
 	Address findById(int addressId);
+
+	boolean deleteAddress(int id);
+
+	Address updateAddress(int id, Address address);
+
+	Address addAddress(Address address);
+
+	Address findByMeetup(Meetup meetup);
+	
+	
 }
+
+
