8f30078a8f44eb6b17f90470db61848a6c5d7551
Ving <ving@Vings-MacBook-Pro.local>
2022-04-29 10:28:45 -0400

    added Test 2 to RecipeCommentTest

--- JPAMocktails/src/test/java/com/skilldistillery/mocktailsmeetup/entities/RecipeCommentTest.java
+++ JPAMocktails/src/test/java/com/skilldistillery/mocktailsmeetup/entities/RecipeCommentTest.java
@@ -16,42 +16,46 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RecipeCommentTest {
-		private static EntityManagerFactory emf;
-		private EntityManager em;
-		private RecipeComment comment;
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private RecipeComment comment;
 
-		@BeforeAll
-		static void setUpBeforeClass() throws Exception {
-			  emf = Persistence.createEntityManagerFactory("JPAMocktails");
-		}
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMocktails");
+	}
 
-		@AfterAll
-		static void tearDownAfterClass() throws Exception {
-			   emf.close();
-		}
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
 
-		@BeforeEach
-		void setUp() throws Exception {
-			 em = emf.createEntityManager();
-			 comment = em.find(RecipeComment.class, 1);
-		}
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		comment = em.find(RecipeComment.class, 1);
+	}
 
-		@AfterEach
-		void tearDown() throws Exception {
-			comment = null;
-			em.close();
-		}
-		
+	@AfterEach
+	void tearDown() throws Exception {
+		comment = null;
+		em.close();
+	}
 
-		@Test
-		@DisplayName("Testing comment mapping")
-		void Test1() {
+	@Test
+	@DisplayName("Testing comment to recipe mapping")
+	void Test1() {
 		assertNotNull(comment);
-		assertEquals("Comment on Shirley Temple", comment.getComment());
-	
-			
-		}
-	
-		
-		}
+		assertEquals("This is one of my favorite mocktails!", comment.getComment());
 
+	}
+
+	@Test
+	@DisplayName("Testing comment to user mapping")
+	void Test2() {
+		assertNotNull(comment);
+		assertEquals("admin@admin.com", comment.getUser().getEmail());
+		assertEquals("admin", comment.getUser().getLastName());
+		assertEquals("admin", comment.getUser().getFirstName());
+	}
+}
